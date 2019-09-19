ac67890cd15261274b3757fe0d638a9e278fc285
Ilya Sokolov <developer.sokolov@gmail.com>
2019-09-19 15:11:36 +0300

    remove comment and add todo

--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 )
 
 func main() {
-	//x := []int{2, 4, 1, 5, 3}
-	//crypt.SetKey(x)
-	crypt.SetStringKey("zabeba")
+	// TODO: Red_byte for test transposition crypt
+	crypt.SetStringKey("java and kotlin")
 	text := crypt.Encrypt("I love coding on go language")
 	fmt.Println(text)
 	fmt.Println(crypt.Decrypt(text))
