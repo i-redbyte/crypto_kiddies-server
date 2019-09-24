cdacf721a59c42e2c8099ff38a4ac5736bb7f689
Ilya Sokolov <developer.sokolov@gmail.com>
2019-09-24 22:34:58 +0300

    shutdown using manners

--- main.go
+++ main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/braintree/manners"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
-func homePage(response http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		http.NotFound(response, req)
-		return
-	}
-	_, err := fmt.Fprint(response, "Crypto kiddies home page")
+func main() {
+	handler := newHandler()
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt, os.Kill)
+	go listenForShutdown(ch)
+	err := manners.ListenAndServe(":4000", handler)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func main() {
-	http.HandleFunc("/", homePage)
-	err := http.ListenAndServe(":4000", nil)
-	if err != nil {
-		panic(err)
+func (h *handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Crypto Punk"
 	}
+	_, _ = fmt.Fprintln(res, "Crypto kiddies home page")
+	_, _ = fmt.Fprint(res, "Hello, ", name)
+
 }
+
+func listenForShutdown(ch <-chan os.Signal) {
+	<-ch
+	manners.Close()
+}
+
+func newHandler() *handler {
+	return &handler{}
+}
+
+type handler struct{}
