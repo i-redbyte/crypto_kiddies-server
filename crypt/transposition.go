a2728c9cb67b773a3e44c86372ad927ff14feeb7
Ilya Sokolov <developer.sokolov@gmail.com>
2019-09-27 16:11:38 +0300

    fix: crypto for utf-8 strings

--- crypt/transposition.go
+++ crypt/transposition.go
@@ -11,33 +11,42 @@ import (
 
 var key []int
 
+type RuneSlice []rune
+
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
 func SetKey(intKey []int) {
 	key = make([]int, len(intKey))
 	copy(key, intKey)
 }
 
 func SetStringKey(stringKey string) {
-	key = make([]int, getLength(stringKey))
+	key = make([]int, GetLength(stringKey))
 	key = getKey(stringKey)
 }
 
-func getKey(word string) []int {
-	word = strings.ToLower(word)
-	sortedWord := strings.Split(word, "")
-	sort.Strings(sortedWord)
+func getKey(keyWord string) []int {
+	keyWord = strings.ToLower(keyWord)
+	word := []rune(keyWord)
+	var sortedWord RuneSlice = make(RuneSlice, len(word))
+	copy(sortedWord, word)
+	sort.Sort(RuneSlice(sortedWord))
 	usedLettersMap := make(map[string]int)
-	wordLength := getLength(word)
+	wordLength := len(word)
 	resultKey := make([]int, wordLength)
 	for i := 0; i < wordLength; i++ {
 		char := word[i]
 		numberOfUsage := usedLettersMap[string(char)]
-		resultKey[i] = getIndex(sortedWord, string(char)) + numberOfUsage + 1 //+1 -so that indexing does not start at 0
+		resultKey[i] = getIndex(sortedWord, char) + numberOfUsage + 1 //+1 -so that indexing does not start at 0
 		numberOfUsage++
 		usedLettersMap[string(char)] = numberOfUsage
 	}
 	return resultKey
 }
-func getIndex(wordSet []string, subString string) int {
+
+func getIndex(wordSet []rune, subString rune) int {
 	n := len(wordSet)
 	for i := 0; i < n; i++ {
 		if wordSet[i] == subString {
@@ -47,23 +56,23 @@ func getIndex(wordSet []string, subString string) int {
 	return 0
 }
 
-func getLength(text string) int {
+func GetLength(text string) int {
 	r := []rune(text)
 	return len(r)
 }
 
-func Encrypt(text string) string {
+func Encrypt(text []rune) string {
+	space := rune(' ')
 	keyLength := len(key)
-	textLength := getLength(text)
+	textLength := len(text)
 	n := textLength % keyLength
 	for i := 0; i < keyLength-n; i++ {
-		text += " "
+		text = append(text, space)
 	}
-	textLength = getLength(text)
+	textLength = len(text)
 	result := ""
-
 	for i := 0; i < textLength; i += keyLength {
-		transposition := make([]byte, keyLength)
+		transposition := make([]rune, keyLength)
 		for j := 0; j < keyLength; j++ {
 			transposition[key[j]-1] = text[i+j]
 		}
@@ -72,13 +81,12 @@ func Encrypt(text string) string {
 	return result
 }
 
-func Decrypt(text string) string {
-	result := ""
-	r := []rune(text)
-	textLength := len(r)
+func Decrypt(text []rune) string {
+	textLength := len(text)
 	keyLength := len(key)
+	result := ""
 	for i := 0; i < textLength; i += keyLength {
-		transposition := make([]byte, keyLength)
+		transposition := make([]rune, keyLength)
 		for j := 0; j < keyLength; j++ {
 			transposition[j] = text[i+key[j]-1]
 		}
