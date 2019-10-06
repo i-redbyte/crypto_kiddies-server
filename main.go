66be5f4aa91413e715de04cfa953da6ec4a7bdb6
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-06 22:19:41 +0300

    add simple handlers

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 	router.Handle("/", http.FileServer(http.Dir("./html/")))
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
 		http.FileServer(http.Dir("./static/"))))
+	router.Handle("/status", StatusHandler).Methods("GET")
+	router.Handle("/cryptos", CryptoHandler).Methods("GET")
+	router.Handle("/cryptos/{slug}/info", GetCryptoHandler).Methods("POST")
+
 	err := http.ListenAndServe(":4000", router)
 	if err != nil {
 		panic(err)
