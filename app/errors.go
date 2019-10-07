4b2b180f12f0f7de027a3b7f9e3c6be07e9d043c
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-08 02:21:28 +0300

    add not found handler error

---
+++ app/errors.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	u "cryptokiddies-server/utils"
+	"net/http"
+)
+
+var NotFoundHandler = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		u.Respond(w, u.Message(false, "Запрашиваемые данные не были найдены на сервере"))
+		next.ServeHTTP(w, r)
+	})
+}
