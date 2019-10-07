d572b3de4ff8f303f92c3162cf4b346412e8083d
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-08 02:20:40 +0300

    move jwt middleware to app

--- handlers.go
+++ handlers.go
@@ -2,16 +2,10 @@ package main
 
 // TODO: split the file into logical components
 import (
-	"context"
-	"cryptokiddies-server/models"
-	u "cryptokiddies-server/utils"
 	"encoding/json"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"net/http"
-	"os"
-	"strings"
 )
 
 type Crypto struct {
@@ -24,67 +18,6 @@ type Crypto struct {
 var cryptos = []Crypto{
 	{1, "Перестановочный шифр", "transposition", "Описание шифра перестановки"},
 	{2, "Шифр Цезаря", "cipher_caesar", "Описание шифра цезаря"},
-}
-
-var JwtMiddleware = func(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		notAuth := []string{"/api/user/registration", "/api/user/login"}
-		requestPath := r.URL.Path
-
-		for _, value := range notAuth {
-			if value == requestPath {
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		response := make(map[string]interface{})
-		tokenHeader := r.Header.Get("Authorization") //Получение токена
-
-		if tokenHeader == "" { //return code 403  Unauthorized
-			response = u.Message(false, "Отсутствует токен авторизации")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
-			return
-		}
-
-		splitted := strings.Split(tokenHeader, " ") //check `Bearer {token-body}`
-		if len(splitted) != 2 {
-			response = u.Message(false, "Invalid/Malformed auth token")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
-			return
-		}
-
-		tokenPart := splitted[1] //get jwt token
-		tk := &models.Token{}
-
-		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("token_password")), nil
-		})
-
-		if err != nil { //return code 403  Unauthorized
-			response = u.Message(false, "Неверно сформированный токен аутентификации")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
-			return
-		}
-
-		if !token.Valid {
-			response = u.Message(false, "Невалидный токен")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
-			return
-		}
-		_ = fmt.Sprintf("User %d", tk.UserId) //Полезно для мониторинга
-		ctx := context.WithValue(r.Context(), "user", tk.UserId)
-		r = r.WithContext(ctx)
-		next.ServeHTTP(w, r)
-	})
 }
 
 var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
