3f5e62f0d5997a0125c3854f756b05daada40542
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-16 22:20:14 +0300

    add getCryptoByPath method

--- model/crypto.go
+++ model/crypto.go
@@ -1,8 +1,20 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Crypto struct {
+	gorm.Model
 	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	Path        string `json:"path"`
 	Description string `json:"description"`
+}
+
+func GetCryptoByPath(path string) *Crypto {
+	crypto := &Crypto{}
+	GetDB().Table("cryptos").Where("path = ?", path).First(crypto)
+	if crypto.Name == "" {
+		return nil
+	}
+	return crypto
 }
