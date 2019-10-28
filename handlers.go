fb7a54a08e143c01233ca5a8fbfd08e80bc0795d
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-29 02:32:27 +0300

    added dummies

--- handlers.go
+++ handlers.go
@@ -10,17 +10,17 @@ var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request
 	_, _ = w.Write([]byte("API is running"))
 })
 
-var GetLogin = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+var GetLogin = func(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "html/login.html")
-})
+}
 
-var PostLogin = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+var PostLogin = func(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 	// TODO: Red_byte test until session
 	fmt.Println(username, password)
 	http.ServeFile(w, r, "html/index.html")
-})
+}
 
 var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Not Implemented"))
