7ea299ee02a723f34a50d43faa1682330b9f90e8
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-30 01:44:09 +0300

    get select decrypt text

--- crypt/selection.go
+++ crypt/selection.go
@@ -24,3 +24,19 @@ func GetCryptoText(slug string, text string, key string) (*string, error) {
 		return nil, errors.New("шифр не найден")
 	}
 }
+
+func GetDecryptText(slug string, text string, key string) (*string, error) {
+	switch slug {
+	case transparent:
+		text, err := trans.Decrypt([]rune(text), key)
+		if err != nil {
+			return nil, err
+		}
+		return &text, nil
+	case caesar:
+		return nil, nil
+	default:
+		return nil, errors.New("текст не найден")
+	}
+
+}
