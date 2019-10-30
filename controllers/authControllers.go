c4fa4e94a449d4fdd93d3ac9e7f5fee2203c39b4
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-30 03:23:03 +0300

    add CreateCryptoTextHandler + small refactoring

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -11,12 +11,12 @@ var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
 	account := &model.Account{}
 	err := json.NewDecoder(r.Body).Decode(account)
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		u.Respond(w, u.Message(false, "Неверный запрос"))
 		return
 	}
 	response := account.CreateAccount()
 	if response["status"] == false {
-		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 	}
 	u.Respond(w, response)
@@ -31,7 +31,6 @@ var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 	}
 	response := model.Login(account.Email, account.Password)
 	if response["status"] == false {
-		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 	}
 	u.Respond(w, response)
