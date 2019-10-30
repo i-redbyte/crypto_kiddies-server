fdd992dc8d74f398e49adedaff778d5e9e51d943
Ilya Sokolov <developer.sokolov@gmail.com>
2019-10-30 03:23:03 +0300

    add CreateCryptoTextHandler + small refactoring

--- controllers/cryptoControllers.go
+++ controllers/cryptoControllers.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"cryptokiddies-server/crypt"
 	"cryptokiddies-server/model"
 	u "cryptokiddies-server/utils"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -10,7 +12,6 @@ import (
 var GetCryptoAlgorithmsHandler = func(w http.ResponseWriter, r *http.Request) {
 	response := u.Message(true, "success")
 	data := model.GetCryptos()
-	w.Header().Set("Content-Type", "application/json")
 	if len(data) == 0 && data == nil {
 		w.WriteHeader(http.StatusNotFound)
 		u.Respond(w, u.Message(false, "Методы шифрования не найдены"))
@@ -26,7 +27,6 @@ var GetCryptoHandler = func(w http.ResponseWriter, r *http.Request) {
 	slug := vars["slug"]
 	crypto = *model.GetCryptoByPath(slug)
 	response := u.Message(true, "success")
-	w.Header().Set("Content-Type", "application/json")
 	if crypto.Slug != "" {
 		response["data"] = crypto
 	} else {
@@ -45,8 +45,38 @@ var GetCryptoTextHandler = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var CreateCryptoTextHandler = func(w http.ResponseWriter, r *http.Request) {
-	// TODO: Red_byte create crypto text by slug from db
+	var crypto model.Crypto
+	gameText := &model.GameText{}
+	err := json.NewDecoder(r.Body).Decode(gameText)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		u.Respond(w, u.Message(false, "Неверный запрос"))
+		return
+	}
+	vars := mux.Vars(r)
+	slug := vars["slug"]
+	text := gameText.Text
+	key := gameText.Key
+
+	crypto = *model.GetCryptoByPath(slug)
+	encryptText, err := crypt.GetCryptoText(slug, text, key)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		u.Respond(w, u.Message(false, err.Error()))
+		return
+	}
+	gameText.AlgorithmName = crypto.Name
+	gameText.AlgorithmId = crypto.Id
+	gameText.Text = *encryptText
+	response := gameText.CreateGameText()
+
+	if response["status"] == false {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	u.Respond(w, response)
 }
+
 var SendAnswerToChef = func(w http.ResponseWriter, r *http.Request) {
 	// TODO: Red_byte send an answer to the chef
 }
