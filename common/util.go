001a391b8fadb576ee95218b5e4bbca1012d4dc3
Ilya Sokolov <developer.sokolov@gmail.com>
2020-04-16 01:07:31 +0300

    @refactor: common files

---
+++ common/util.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/ilya-sokolov/crypto_kiddies-server/check"
+	"net/http"
+)
+
+func BindAndValidate(ctx *gin.Context, r interface{}) error {
+	err := ctx.BindJSON(&r)
+	if err != nil {
+		ResponseError(ctx, http.StatusBadRequest, ErrorMessage{Message: err.Error()})
+		return err
+	}
+	err = check.Struct(r)
+	if err != nil {
+		ResponseValidationError(ctx, err.(validator.ValidationErrors))
+		return err
+	}
+	return nil
+}
